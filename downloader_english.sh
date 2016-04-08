89123baf7b60dc4533a6ac5660f065101309c2d0
akushsky <akushsky@it-universe.ru>
2016-04-08 16:29:52 +0300

    Oops, strange dot

--- downloader_english.sh
+++ downloader_english.sh
@@ -3,7 +3,7 @@ for i in {1..1032}
 do
     number=`printf %04d ${i%.*}`
     url="http://www.giantitp.com/comics/oots$number.html"
-    image_url="http://www.giantitp.com/".`curl -s "$url" | grep /comics/images/ | cut -d'"' -f4`
+    image_url="http://www.giantitp.com"`curl -s "$url" | grep /comics/images/ | cut -d'"' -f4`
     extension=`echo "$image_url" | cut -d'.' -f5`
     wget -c -q -O "oots$number.$extension" $image_url
     echo "Downloading: $image_url"
